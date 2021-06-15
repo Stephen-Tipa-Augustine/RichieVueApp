cae9a05d63d01f555075c0d7382691220733b21d
Stephen Tipa Augustine <tipastep5@gmail.com>
2021-06-15 13:01:20 +0300

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <hr />
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <p>
+            This is a simple start of the bootstrap code
+          </p>
+        </div>
+        <div class="col-sm-6">
+          <p>
+            Still showing you how this staff is done bro
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
